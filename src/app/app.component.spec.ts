30486457cf5704df812aefd7d5c91af1c80a811e
Vahe <vaegev1@gmail.com>
2024-10-01 18:15:39 +0400

    TNK-4 Build UI for Login Page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tnakan' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tnakan');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatLabel,
+    MatError,
+    MatButton,
+    TranslateModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  emailErrorMessage = signal('');
+  passErrorMessage = signal('');
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    merge(this.email!.statusChanges, this.email!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+    merge(this.password!.statusChanges, this.password!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email!.hasError('required')) {
+      this.emailErrorMessage.set('required');
+    } else if (this.email!.hasError('email')) {
+      this.emailErrorMessage.set('email');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+
+    if (this.password!.hasError('required')) {
+      this.passErrorMessage.set('required');
+    } else if (this.password!.hasError('minlength')) {
+      this.passErrorMessage.set('password');
+    } else {
+      this.passErrorMessage.set('');
+    }
+  }
 }
