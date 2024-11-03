d90ec369e07068cdad15ab586ebf6c86b48f8be6
artur <artursahakyan1992@gmail.com>
2024-11-03 23:59:22 +0400

    sidebar functionality

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,92 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDrawerContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarHttpService } from '../shared/services/sidebar-http.service';
+import { CategoriesService } from '../shared/services/categories.service';
+import { forkJoin } from 'rxjs';
+
+export interface SideBarMenu {
+  categoryName: string;
+  categoryId: string;
+  selected?: boolean;
+  categories: SideBarMenu[];
+}
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem,
+    MatDrawerContainer,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCheckbox,
+    FormsModule,
+    MatIcon,
+    NavbarComponent,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+})
+export class SidebarComponent implements OnInit {
+  isOpen: boolean = false;
+  menuList: SideBarMenu[] = [];
+
+  sidebarHttpService = inject(SidebarHttpService);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getCategoriesData();
+  }
+
+  private getCategoriesData(): void {
+    forkJoin([
+      this.sidebarHttpService.getCategoriesList(),
+      this.sidebarHttpService.getSubCategoriesList(),
+      this.sidebarHttpService.getProductCategoriesList(),
+    ]).subscribe(([categories, subCategories, productCategories]) => {
+      this.menuList = categories.map((category: any) => {
+        return {
+          categoryName: category.name,
+          categoryId: category.id,
+          selected: false,
+          categories: subCategories
+            .filter((subCategory: any) => subCategory.category_id === category.id)
+            .map((subCategory1: any) => {
+              return {
+                categoryName: subCategory1.name,
+                categoryId: subCategory1.id,
+                selected: false,
+                categories: productCategories
+                  .filter(
+                    (productCategory: any) => productCategory.sub_category_id === subCategory1.id
+                  )
+                  .map((productCategoryItem: any) => {
+                    return {
+                      categoryName: productCategoryItem.name,
+                      categoryId: productCategoryItem.id,
+                      selected: false,
+                    };
+                  }) as SideBarMenu[],
+              };
+            }) as SideBarMenu[],
+        };
+      });
+    });
+  }
+
+  openSub(event: MouseEvent, menuItem: any) {
+    this.isOpen = !this.isOpen;
+    menuItem.selected = !menuItem.selected;
+  }
+}
