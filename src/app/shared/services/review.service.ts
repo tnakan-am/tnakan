7b79b9c01f280f8a6c36962a3ac0d0fb2127429b
Vahe <vaegev1@gmail.com>
2024-11-15 19:27:50 +0400

    TNK-35 Review changes

--- src/app/shared/services/review.service.ts
+++ src/app/shared/services/review.service.ts
@@ -4,7 +4,8 @@ import { FirebaseAuthService } from './firebase-auth.service';
 import { ProductsService } from './products.service';
 import { OrderItem } from '../interfaces/order.interface';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
-import { Observable } from 'rxjs';
+import { catchError, filter, Observable, switchMap, throwError } from 'rxjs';
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,32 +13,46 @@ import { Observable } from 'rxjs';
 export class ReviewService {
   firestore: Firestore = inject(Firestore);
   firebaseAuthService = inject(FirebaseAuthService);
+  userService = inject(UsersService);
   productsService = inject(ProductsService);
 
   constructor() {}
 
   writeReview(product: OrderItem, review: { stars: number; comment: string }) {
     const userId = this.firebaseAuthService.getFirebaseUser()?.uid!;
-
-    const reviewRef = doc(this.firestore, 'reviews', product.id);
-
-    return fromPromise(
-      setDoc(
-        reviewRef,
-        {
-          [product.orderId!]: { ...review, userId },
-        },
-        { merge: true }
-      )
+    return this.userService.getUserData().pipe(
+      filter((user) => !!user),
+      switchMap((user) => {
+        const reviewRef = doc(this.firestore, 'reviews', product.id);
+        return fromPromise(
+          setDoc(
+            reviewRef,
+            {
+              [product.orderId!]: {
+                ...review,
+                userId,
+                userPhoto: user?.image || null,
+                userName: user?.displayName,
+              },
+            },
+            { merge: true }
+          ).then(() => ({ ...review }))
+        );
+      }),
+      catchError((err) => throwError(err))
     );
   }
 
-  getOrderReview(product: OrderItem): Observable<any> {
-    const reviewRef = doc(this.firestore, 'reviews', product.id);
+  getProductReview(product: Partial<OrderItem>): Observable<any> {
+    const reviewRef = doc(this.firestore, 'reviews', product.id!);
 
     return fromPromise(
       getDoc(reviewRef).then((docSnap) =>
-        docSnap.exists() ? docSnap.data()[product.orderId!] : null
+        docSnap.exists()
+          ? product.orderId
+            ? docSnap.data()[product.orderId!]
+            : Object.keys(docSnap.data()).map((key) => ({ ...docSnap.data()[key], id: key }))
+          : null
       )
     );
   }
