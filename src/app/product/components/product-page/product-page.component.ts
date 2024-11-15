cb131258396a89ac1444294a30d419a121bef4d1
Vahe <vaegev1@gmail.com>
2024-11-15 20:13:04 +0400

    TNK-36 Product category_ref fixes

--- src/app/product/components/product-page/product-page.component.ts
+++ src/app/product/components/product-page/product-page.component.ts
@@ -57,7 +57,6 @@ export class ProductPageComponent implements OnInit {
   getProductItem(productId: string): void {
     this.productsService.getProductById(productId).subscribe((product) => {
       this.product = product;
-      console.log(this.product);
     });
   }
 
