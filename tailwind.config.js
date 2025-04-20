85c2a4e87283efc1a2097d18bb1c25e638d7da43
Vahe <vaegev1@gmail.com>
2025-04-20 14:34:46 +0400

    TNK-65 Remove PostCSS and lockfile configuration
    Deleted `.postcssrc.json` and `package-lock.json` to clean up unused PostCSS configurations and package dependencies. This reduces clutter and ensures a more streamlined configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts,scss,css}', './src/**/*.component.{html,ts,scss,css}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
